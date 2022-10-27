deabce1ed5af472e9488d643a1a4d384de5edc5c
Thomas Le Bihan <thomas.66days@gmail.com>
2022-10-27 12:03:39 +0200

    add portable text and post body

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,13 +2,17 @@ import { GetStaticProps } from 'next'
 import Header from '../../components/Header'
 import { client, urlFor } from '../../lib/sanity'
 import { Post } from '../../typings'
+import PortableText  from 'react-portable-text'
 
 interface PostProps {
   post: Post
 }
 
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!
+
 const Post = ({ post }: PostProps) => {
-  console.log(post)
+  console.log(projectId)
   return (
     <main>
       <Header />
@@ -21,6 +25,7 @@ const Post = ({ post }: PostProps) => {
       <article className='max-w-3xl mx-auto p-5'>
         <h1 className='text-3xl mt-10 mb-3' >{post.title}</h1>
         <h2 className='text-xl font-light text-gray-500 mb-2' >{post.description}</h2>
+        
         <div className='flex items-center space-x-2'>
           <img 
             className='h-10 w-10 rounded-full'
@@ -31,6 +36,34 @@ const Post = ({ post }: PostProps) => {
             Blog post by <span className='text-green-600'>{post.author.name}</span> - Published at { new Date(post._createdAt).toLocaleString()}
           </p>
         </div>
+
+        <div className='mt-10' >
+        <PortableText 
+            className=''
+            dataset={dataset}
+            projectId={projectId}
+            content={post.body}
+            serializers={
+              {
+                h1: (props: any) => {
+                  <h1 className='text-2xl font-bold my-5' { ...props } />
+                },
+                h2: (props: any) => {
+                  <h2 className='text-xl font-bold my-5' { ...props } />
+                },
+                li: ({ children }: any) => {
+                  <li className='text-2xl font-bold my-5'>{ children }</li>
+                },
+                link: ({ href, children }: any) => {
+                  <a href={href} className='text-blue-500 hover:underline'>
+                    { children }
+                  </a>
+                },
+              }
+            }
+          />
+        </div>
+
       </article>
     </main>
   )
@@ -88,7 +121,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     props: {
       post,
     },
-    revalidate: 6000,
+    revalidate: 60,
   };
 };
 
